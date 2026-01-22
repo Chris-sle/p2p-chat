746dd167113197233240720b2acd5ee631dcb26a
Chris <oscar.sletvold@gmail.com>
2026-01-22 21:26:16 +0100

    Update p2pnet.h

--- c-lib/include/p2pnet/p2pnet.h
+++ c-lib/include/p2pnet/p2pnet.h
@@ -11,7 +11,21 @@
 #include "p2pnet/message.h"
 #include "p2pnet/event_loop.h"
 #include "p2pnet/crypto.h" 
+#include "p2pnet/session.h"
+#include "p2pnet/handshake.h"
 // Flere headers kommer senere (event_loop.h, etc.)
+
+/**
+ * Initialize P2PNet library (Winsock + libsodium)
+ * 
+ * @return 0 on success, -1 on error
+ */
+int p2p_init(void);
+
+/**
+ * Cleanup P2PNet library
+ */
+void p2p_cleanup(void);
 
 
 #endif /* P2PNET_H */
